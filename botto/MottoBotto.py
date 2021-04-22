bb11ae4ee455f029abf8243a788d14a18ea8bf9b
Ben Cardy <benbacardi@gmail.com>
2021-04-22 10:10:48 +0100

    DM to change leaderboard emoji: fixes #32

--- botto/MottoBotto.py
+++ botto/MottoBotto.py
@@ -1,6 +1,7 @@
 import logging
 import re
 from typing import Optional
+from emoji import UNICODE_EMOJI
 
 import discord
 from airtable import Airtable
@@ -31,14 +32,6 @@ class MottoBotto(discord.Client):
         )
         log.info("Responding to phrases: %s", self.config["triggers"])
 
-        # Uppercase the triggers as uppercase is more friendly to other languages
-        # This is in code to reduce the cognitive load on end users
-        for trigger_group in self.config["triggers"]:
-            uppercased_trigger_group = []
-            for trigger in self.config["triggers"][trigger_group]:
-                uppercased_trigger_group.append(trigger.upper())
-            self.config["triggers"][trigger_group] = uppercased_trigger_group
-
         intents = discord.Intents(messages=True, members=True, guilds=True)
         super().__init__(intents=intents)
 
@@ -52,12 +45,11 @@ class MottoBotto(discord.Client):
             await message.add_reaction(reaction)
 
     async def on_message(self, message: Message):
+
         if is_dm(message):
-            log.info(
-                f"Received direct message (ID: {message.id}) from {message.author}: {message.content}"
-            )
-            log.info(f"Ignored message {message.id}")
+            await self.process_dm(message)
             return
+
         channel_name = message.channel.name
 
         if (
@@ -69,17 +61,7 @@ class MottoBotto(discord.Client):
             if channel_name in self.config["channels"]["exclude"]:
                 return
 
-        if message.content.upper() not in self.config["triggers"]["new_motto"]:
-            return
-
-        if is_botto(message, self.user):
-            await reactions.skynet_prevention(self, message)
-        elif not message.reference:
-            await reactions.not_reply(self, message)
-        elif not self.is_valid_message(message.reference.resolved):
-            await reactions.invalid(self, message)
-        else:
-            await self.process_suggestion(message)
+        await self.process_suggestion(message)
 
     async def on_member_update(self, before: Member, after: Member):
         member_record = self.update_existing_member(after)
@@ -111,16 +93,25 @@ class MottoBotto(discord.Client):
             return False
         return True
 
-    async def get_or_add_member(self, member: Member):
+    async def get_or_add_member(self, member: Member, emoji: Optional[str] = None):
         member_record = self.members.match("Discord ID", member.id)
+        data = {
+            "Name": member.display_name,
+        }
+        if emoji is not None:
+            data["Emoji"] = emoji
+
         if not member_record:
-            member_data = {"Name": member.display_name, "Discord ID": str(member.id)}
-            member_record = self.members.insert(member_data)
+            data["Discord ID"] = str(member.id)
+            member_record = self.members.insert(data)
             log.debug(f"Added member {member_record} to AirTable")
-       
+        elif emoji is not None and member_record["fields"].get("Emoji", "") != emoji:
+            log.debug("Updating member details")
+            self.members.update(member_record["id"], data)
+
         return member_record
 
-    def update_existing_member(self, member: Member) -> Optional[dict]:
+    def update_existing_member(self, member: Member, emoji: Optional[str] = None) -> Optional[dict]:
         """
         Updates an existing member's record. This will not add new members
         :param member: the updated member from Discord
@@ -129,11 +120,32 @@ class MottoBotto(discord.Client):
         member_record = self.members.match("Discord ID", member.id)
         if not member_record:
             return None
-        self.members.update(member_record["id"], {"Name": member.display_name})
+        update_dict = {
+            "Name": member.display_name,
+        }
+        if emoji is not None:
+            update_dict["Emoji"] = emoji
+        self.members.update(member_record["id"], update_dict)
         return member_record
 
     async def process_suggestion(self, message: Message):
+
+        if message.content.upper() not in self.config["triggers"]["new_motto"]:
+            return
+
+        if is_botto(message, self.user):
+            await reactions.skynet_prevention(self, message)
+            return
+
+        if not message.reference:
+            await reactions.not_reply(self, message)
+            return
+
         motto_message: Message = message.reference.resolved
+
+        if not self.is_valid_message(motto_message):
+            await reactions.invalid(self, message)
+            return
 
         if motto_message.author == message.author:
             await reactions.fishing(self, message)
@@ -166,3 +178,32 @@ class MottoBotto(discord.Client):
         log.debug("Added Motto to AirTable")
 
         await reactions.stored(self, message, motto_message)
+
+    async def process_dm(self, message: Message):
+
+        log.info(
+            f"Received direct message (ID: {message.id}) from {message.author}: {message.content}"
+        )
+
+        upper_content = message.content.upper()
+
+        if emoji_trigger := [t for t in self.config["triggers"]["change_emoji"] if upper_content.startswith(t)]:
+
+            emoji_trigger = emoji_trigger[0]
+            content = message.content[len(emoji_trigger):].strip().strip('\ufe0f')
+            log.debug(f"User {message.author} wants to change emoji: {content!r} (trigger: {emoji_trigger})")
+
+            if not content:
+                log.debug(f"Removing emoji")
+                await self.get_or_add_member(message.author, emoji="")
+                await reactions.valid_emoji(self, message)
+            elif content in UNICODE_EMOJI["en"]:
+                log.debug(f"Updating emoji")
+                await self.get_or_add_member(message.author, emoji=content)
+                await reactions.valid_emoji(self, message)
+            else:
+                await reactions.invalid_emoji(self, message)
+
+            return
+
+        await reactions.unknown_dm(self, message)
