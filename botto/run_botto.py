dfa46a08352a930ad1e4f41ae1227ac2f5e9f866
David Stephens <git@dwrs.org>
2021-05-04 16:14:40 +0100

    Log INFO-level messages from `discord.gateway`

--- botto/run_botto.py
+++ botto/run_botto.py
@@ -11,6 +11,7 @@ from config import parse
 # Configure logging
 logging.config.fileConfig(fname="log.conf", disable_existing_loggers=False)
 logging.getLogger("discord").setLevel(logging.CRITICAL)
+logging.getLogger("discord.gateway").setLevel(logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("urllib").setLevel(logging.CRITICAL)
 log = logging.getLogger("MottoBotto")
