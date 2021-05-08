4780c6d6c08da08ed502232519eaf4ea186e72f2
David Stephens <git@dwrs.org>
2021-05-08 16:27:43 +0100

    Add AirTableError exception.

--- botto/models.py
+++ botto/models.py
@@ -113,3 +113,24 @@ class Member(Model):
             emoji = f"{self.emoji} "
         name = self.nickname if self.nickname and self.use_nickname else self.username
         return f"{emoji}{name}"
+
+
+class AirTableError(Exception):
+    def __init__(self, response_dict: dict, *args: object) -> None:
+        error_dict: dict = response_dict["error"]
+        self.error_type = error_dict.get("type")
+        self.error_message = error_dict.get("message")
+        super().__init__(*args)
+
+    def __repr__(self) -> str:
+        return "{class_name}(type:{error_type}, message:{error_message})".format(
+            class_name=self.__class__,
+            error_type=self.error_type,
+            error_message=self.error_message
+        )
+
+    def __str__(self) -> str:
+        return "Error from AirTable operation of type '{error_type}', with message:{error_message}".format(
+            error_type=self.error_type,
+            error_message=self.error_message
+        )
