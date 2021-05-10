ceb60d2a48fdcaa9002cb2c014bddff862680434
David Stephens <git@dwrs.org>
2021-05-10 11:23:31 +0100

    A more selective (read: Absurdly overcomplicated) regex for matching apologies

--- botto/regexes.py
+++ botto/regexes.py
@@ -33,7 +33,21 @@ def compile_regexes(bot_user_id: str) -> SuggestionRegexes:
         trigger=[re.compile(rf"^{self_id}")],
         pokes=re.compile(rf"pokes? {self_id}", re.IGNORECASE),
         sorry=re.compile(rf"sorry,? {self_id}", re.IGNORECASE),
-        apologising=re.compile(rf"sorry|apologi([zs]e|es)", re.IGNORECASE),
+        apologising=re.compile(
+            rf"""(?:I['"m]+|my|^) # Match I/I'm/My
+            [ ]* # Match any number of spaces
+            (?:
+              (?:
+                (?:sincer|great) # Matching the start of sincere/great
+                (?:est|e(?:ly)?)? # Match the end of sincerest/sincere/sincerely
+                |so|very|much|such
+              )
+            .?)* # Match any number of "sincerely", "greatest", "so" etc. with or without characters in between
+            [ ]* # Match any number of spaces
+            (sorry|apologi([zs]e|es)) # Match sorry/apologise/apologies,etc.
+        """,
+            re.IGNORECASE|re.VERBOSE,
+        ),
         off_topic=re.compile(rf"off( +|\-)topic", re.IGNORECASE),
         love=re.compile(rf"I love( you,?)? {self_id}", re.IGNORECASE),
         band=re.compile(
