03eb236aced8d87488993530c284929a6d8f75cc
David Stephens <git@dwrs.org>
2021-05-23 15:51:15 +0100

    Convert remaining "Motto" actions to run async

--- botto/models.py
+++ botto/models.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from dateutil import parser
+from yarl import URL
 
 
 class Model:
@@ -116,21 +119,28 @@ class Member(Model):
 
 
 class AirTableError(Exception):
-    def __init__(self, response_dict: dict, *args: object) -> None:
+    def __init__(
+        self, url: URL, response_dict: Union[dict, str], *args: object
+    ) -> None:
         error_dict: dict = response_dict["error"]
-        self.error_type = error_dict.get("type")
-        self.error_message = error_dict.get("message")
+        self.url = url
+        if type(response_dict) is dict:
+            self.error_type = error_dict.get("type")
+            self.error_message = error_dict.get("message")
+        else:
+            self.error_type = error_dict
+            self.error_message = ""
         super().__init__(*args)
 
     def __repr__(self) -> str:
-        return "{class_name}(type:{error_type}, message:{error_message})".format(
+        return "{class_name}(type:{error_type}, message:'{error_message}', url:{url})".format(
             class_name=self.__class__,
             error_type=self.error_type,
-            error_message=self.error_message
+            error_message=self.error_message,
+            url=self.url,
         )
 
     def __str__(self) -> str:
-        return "Error from AirTable operation of type '{error_type}', with message:{error_message}".format(
-            error_type=self.error_type,
-            error_message=self.error_message
+        return "Error from AirTable operation of type '{error_type}', with message:'{error_message}'. Request URL: {url}".format(
+            error_type=self.error_type, error_message=self.error_message, url=self.url
         )
