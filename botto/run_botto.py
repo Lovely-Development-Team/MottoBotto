d72bf4a462cd961e597dcf4b704427500d79ff6e
Dom Chester <dom.chester@me.com>
2023-11-08 18:29:20 +0300

    Lower discordpy error logging
    DiscordPy catches all errors being handled so setting to critical hides errors we might be generating

--- botto/run_botto.py
+++ botto/run_botto.py
@@ -8,7 +8,7 @@ from config import parse
 
 # Configure logging
 logging.config.fileConfig(fname="log.conf", disable_existing_loggers=False)
-logging.getLogger("discord").setLevel(logging.CRITICAL)
+logging.getLogger("discord").setLevel(logging.ERROR)
 logging.getLogger("discord.gateway").setLevel(logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("urllib").setLevel(logging.CRITICAL)
